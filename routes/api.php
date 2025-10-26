c800ff6e283567ef6c20d34ebd18ab9e57503fbe
fernandom11399 <139576221+fernandom11399@users.noreply.github.com>
2025-10-25 18:36:35 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\ConsumtionController;
+use App\Http\Controllers\api\ProductContrller;
+use App\Http\Controllers\api\ProductivityController;
+use App\Models\Consumption;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +21,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('auth')->controller(AuthController::class)->group(function() {
+    Route::get('register', "register");
+    Route::get('login', "login");
+    Route::get('logout', "logout");
+    Route::get('about-me', "abotuMe");
+});
+
+
+Route::prefix('product')->controller(ProductContrller::class)->group(function() {
+    Route::get('/', "index");
+    Route::get('/{id}', "search")->where("id","[0-9]+");
+    Route::delete('/{id}', "search")->where("id","[0-9]+");
+});
+
+Route::prefix('consumption')->controller(ConsumtionController::class)->group(function() {
+    Route::get('/', "index");
+    Route::get('/{id}', "search")->where("id","[0-9]+");
+    Route::delete('/{id}', "search")->where("id","[0-9]+");
+});
+
+Route::prefix('productivity')->controller(ProductivityController::class)->group(function() {
+    Route::get('/', "index");
+    Route::get('/{id}', "search")->where("id","[0-9]+");
+    Route::delete('/{id}', "search")->where("id","[0-9]+");
 });
