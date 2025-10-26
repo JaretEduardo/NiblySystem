c3729e59c3c9f176fcf029d97c4d8db9f18694f4
fernandom11399 <139576221+fernandom11399@users.noreply.github.com>
2025-10-25 18:00:59 -0600

    fix: RD products
    Se hizo el búsqueda, leer y eliminar de los registros de productos

---
+++ app/Http/Controllers/api/ProductContrller.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class ProductContrller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Products::all();
+        if(count($products) <= 0){
+            return response()->json([
+                'result' => false,
+                'msg' => "Por el momento no hay productos registrados.",
+                'data' => null
+            ], 404);
+        }
+        return response()->json([
+            'result' => true,
+            'msg' => "Productos encontrados.",
+            'data' => $products
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        $product = Products::find($id);
+        if(!$product){
+            return response()->json([
+                'result' => false,
+                'msg' => "No se encontro el recurso buscado.",
+                'data' => null
+            ], 404);
+        }
+        return response()->json([
+            'result' => true,
+            'msg' => "Se ha encontrado los datos de " . $product->Product_Name .".",
+            'data' => $product
+        ], 202);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Products::find($id);
+        if(!$product){
+            return response()->json([
+                'result' => false,
+                'msg' => "No se encontro el recurso buscado.",
+                'data' => null
+            ], 404);
+        }
+        return response()->json([
+            'result' => true,
+            'msg' => "Se ha actualizado los datos de " . $product->Product_Name .".",
+            'data' => $product
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product = Products::find($id);
+        if(!$product){
+            return response()->json([
+                'result' => false,
+                'msg' => "No se encontro el recurso buscado.",
+                'data' => null
+            ], 404);
+        }
+        $product->delete();
+        return response()->json([
+            'result' => true,
+            'msg' => "Se ha eliminado el registro de " . $product->Product_Name .".",
+            'data' => null
+        ], 202);
+    }
+}
